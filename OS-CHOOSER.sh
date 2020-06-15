3d9e08fdeb02a06a4f3a349eaeec4cba53b8d8c5
raspberrypisig <raspberrypisig@gmail.com>
2020-06-15 12:34:33 -0400

    Update OS-CHOOSER.sh

--- OS-CHOOSER.sh
+++ OS-CHOOSER.sh
@@ -15,6 +15,7 @@ SACRIFICIAL_BOOTPARTITION="${USB_BLOCK_DEVICE}${SACRIFICIAL_BOOTPARTITION_NUMBER
 TEMP_DIR=/tmp
 SACRIFICIAL_MOUNT_DIR="${TEMP_DIR}/boot"
 TARGET_MOUNT_DIR="${TEMP_DIR}/targetboot"
+KERNEL_NAME="kernel7l.img"
 
 declare -A oslist
 options=()
@@ -49,7 +50,7 @@ mount "$SACRIFICIAL_BOOTPARTITION" $SACRIFICIAL_MOUNT_DIR
 mount "${USB_BLOCK_DEVICE}$bootpart" $TARGET_MOUNT_DIR
 ESCAPED_USB_BLOCK_DEVICE=$(sed 's/\//\\\//g' <<< $USB_BLOCK_DEVICE)
 sed -i -r "s/root=([^ ]*) /root=${ESCAPED_USB_BLOCK_DEVICE}\/${linuxpart} /" "${SACRIFICIAL_MOUNT_DIR}/cmdline.txt"
-cp "${TARGET_MOUNT_DIR}/kernel7l.img" "${SACRIFICIAL_MOUNT_DIR}/${bootpart}.img"
+cp "${TARGET_MOUNT_DIR}/$KERNEL_NAME" "${SACRIFICIAL_MOUNT_DIR}/${bootpart}.img"
 sed -i -r "s/kernel=(.*)$/kernel=${bootpart}.img/" "${SACRIFICIAL_MOUNT_DIR}/config.txt"
 umount "${SACRIFICIAL_MOUNT_DIR}"
 umount "${TARGET_MOUNT_DIR}"
