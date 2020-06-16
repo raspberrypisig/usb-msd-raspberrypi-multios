b5cb9636e962992ebd112770ec260fb69e4f576c
raspberrypisig <raspberrypisig@gmail.com>
2020-06-16 07:36:43 -0400

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -5,4 +5,5 @@ sudo systemctl enable OS-CHOOSER
 sudo systemctl disable dhcpcd
 sudo systemctl disable rpi-eeprom-update
 sudo systemctl disable avahi-daemon
+sudo systemctl mask raspi-config
 sudo reboot
