0709ccc27b8f40cba32c480b2f82527ef367c77c
raspberrypisig <raspberrypisig@gmail.com>
2020-06-17 07:41:12 -0400

    Update multipi4.sh

--- multipi4.sh
+++ multipi4.sh
@@ -4,7 +4,7 @@ set -x
 setup() {
   usbdevice="$1"
   raspbianliteimg="$2"
-  
+
   loopdevice=$(losetup --show -Pf "$raspbianliteimg")
   mkdir -p raspbianboot
   mkdir -p raspbian
@@ -14,9 +14,9 @@ setup() {
   mount ${usbdevice}1 usb1
   mount ${usbdevice}2 usb2
   mount ${usbdevice}3 usb3
-  
-  cp -r raspbianboot/* usb1/ 
-  rsync -au raspbian/ usb3/
+
+  cp -r raspbianboot/* usb1/
+  rsync -a raspbian/ usb3/
   wget -O usb3/etc/fstab https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/raw/master/fstab
   wget https://github.com/raspberrypi/rpi-eeprom/releases/download/v2020.05.28-137ad/usb-msd-boot-firmware.zip
   unzip -d usb1 usb-msd-boot-firmware.zip
@@ -24,17 +24,18 @@ setup() {
   wget -O usb1/cmdline.txt https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/raw/master/cmdline.txt
   cp -r usb1/* usb2/
   wget -O usb2/config.txt https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/raw/master/partition2/config.txt
-  
-  umount {usb1,usb2,usb3, raspbianboot, raspbian}
+
+  umount {usb1,usb2,usb3,raspbianboot,raspbian}
   losetup -D $loopdevice
 }
+
 
 addos() {
   osimg="$1"
   bootpart="$2"
   linuxpart="$3"
   name="$4"
-  
+
   loopdevice=$(losetup --show -Pf "$osimg")
   mkdir -p osboot
   mkdir -p os
@@ -44,16 +45,30 @@ addos() {
   mount -o ro ${loopdevice}p2 os
   mount $bootpart usbboot
   mount $linuxpart usblinux
-  
+
   cp -r osboot/* $usbboot
-  rsync -au usblinux/ $usblinux
+  rsync -a usblinux/ $usblinux
   wget -O $usblinux/etc/fstab https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/blob/master/fstab
   sed -i -r "s/\/dev\/sda1/$bootpart/" $usblinux/etc/fstab
   sed -i -r "s/\/dev\/sda2/$linuxpart/" $usblinux/etc/fstab
-  
+
   umount {usbboot,usblinux,osboot,os}
   losetup -D $loopdevice
 }
 
 
+case "$1" in
+  "setup")
+    shift
+    setup "$@"
+   ;;
+  "addon")
+    shift
+    addon "$@"
+   ;;
+  *)
+    echo help
+    ;;
 
+
+esac
