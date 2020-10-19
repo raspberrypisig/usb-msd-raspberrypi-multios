d48e489e6beb2566da1654f454996657713c597f
raspberrypisig <raspberrypisig@gmail.com>
2020-10-19 19:48:08 -0400

    Update emtpydisk.sh

--- emtpydisk.sh
+++ emtpydisk.sh
@@ -2,4 +2,10 @@
 
 usb_disk="$1"
 
-echo 'label: gpt' | sfdisk $usb_disk 
+sfdisk $usb_disk <<EOF
+label: gpt
+,200MiB,EBD0A0A2-B9E5-4433-87C0-68B6B72699C7
+,10GiB,EBD0A0A2-B9E5-4433-87C0-68B6B72699C7
+,,0FC63DAF-8483-4772-8E79-3D69D8477DE4
+EOF
+
