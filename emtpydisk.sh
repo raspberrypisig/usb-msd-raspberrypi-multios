e72f4c0cfe4dc616237fce5e91d368620e03be1f
raspberrypisig <raspberrypisig@gmail.com>
2020-10-19 22:57:58 -0400

    Update emtpydisk.sh

--- emtpydisk.sh
+++ emtpydisk.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -x
 
-usb_disk="$1"
+raspbian_image="$1"
+usb_disk="$2"
 
 sfdisk $usb_disk <<EOF
 label: gpt
@@ -9,9 +11,21 @@ label: gpt
 ,,0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
 
-mkfs.vfat "${usb_disk}/p1"
-mkfs.vfat "${usb_disk}/p2"
-mkfs.btrfs "${usb_disk}/p3"
+mkfs.vfat "${usb_disk}p1"
+mkfs.vfat "${usb_disk}p2"
+mkfs.btrfs -f "${usb_disk}p3"
 
+loop=$(losetup --show -Pf $raspbian_image)
 
+mkdir -p {p1,p2,usb1,usb3}
+mount ${loop}p1 p1
+mount ${loop}p2 p2
+mount ${usb_disk}p1 usb1
+mount ${usb_disk}p3 usb3
+cd usb3
+btrfs subvolume create 1
+cd ..
+cp -rv p1/* usb1
+rsync -avu p2/ usb3/1
 
+losetup -D $loop
