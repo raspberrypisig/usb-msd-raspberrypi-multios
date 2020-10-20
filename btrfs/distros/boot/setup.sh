533ad4c2aeab7c9bb0b97860df6051f2c3e1c859
raspberrypisig <raspberrypisig@gmail.com>
2020-10-20 18:27:30 -0400

    Update setup.sh

--- btrfs/distros/boot/setup.sh
+++ btrfs/distros/boot/setup.sh
@@ -3,3 +3,6 @@
 sudo apt install -y initramfs-tools btrfs-tools
 echo btrfs >> /etc/initramfs-tools/modules	
 mkinitramfs -o /boot/initramfs-btrfs.gz
+# Disable kernel updates
+sudo apt-mark hold libraspberrypi-bin libraspberrypi-dev libraspberrypi-doc libraspberrypi0
+sudo apt-mark hold raspberrypi-bootloader raspberrypi-kernel raspberrypi-kernel-headers
