2141ac9d6ed9257175ea9e2fc13e6bd108b7e3d7
raspberrypisig <raspberrypisig@gmail.com>
2020-10-21 22:22:21 +0000

    Update preparestartos.sh

--- btrfs/preparestartos.sh
+++ btrfs/preparestartos.sh
@@ -4,12 +4,12 @@ disk="$2"
 bootfiles=distros/boot/files/fat32
 linuxfiles=distros/boot/files/btrfs
 
-mkdir -p {usb1,usb3}
+mkdir -p usb3
 
-mount ${disk}p1 usb1
 mount ${disk}p3 usb3
+mount ${disk}p1 usb3/boot
 
-cp -rv $bootfiles/* usb1
+cp -rv $bootfiles/* usb3/boot
 rsync -av $linuxfiles/ usb3
 
 apt install -y qemu qemu-user-static binfmt-support systemd-container
@@ -18,6 +18,6 @@ cp setup.sh usb3
 systemd-nspawn -D usb3 /setup.sh 
 rm usb3/setup.sh
 
-umount {usb1,usb3}
-rm -rf {usb1,usb3}
+umount {usb3/boot, usb3}
+rm -rf usb3
 
