82feb4726e4aa37a799255bc6c5c63ff5a22a0a4
raspberrypisig <raspberrypisig@gmail.com>
2020-10-22 21:49:06 +0000

    Update preparestartos.sh

--- btrfs/preparestartos.sh
+++ btrfs/preparestartos.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 disk="$1"
-bootfiles=distros/boot/files/fat32
-linuxfiles=distros/boot/files/btrfs
+bootfiles=usb-msd-raspberrypi-multios/btrfs/distros/boot/files/fat32
+linuxfiles=usb-msd-raspberrypi-multios/btrfs/distros/boot/files/btrfs
+
+apt install -y qemu qemu-user-static binfmt-support systemd-container
+git clone https://github.com/raspberrypisig/usb-msd-raspberrypi-multios
 
 mkdir -p usb3
 
@@ -12,7 +15,6 @@ mount ${disk}p1 usb3/boot
 cp -rv $bootfiles/* usb3/boot
 rsync -av $linuxfiles/ usb3
 
-apt install -y qemu qemu-user-static binfmt-support systemd-container
 chmod +x setup.sh
 cp setup.sh usb3
 systemd-nspawn -D usb3 /setup.sh 
