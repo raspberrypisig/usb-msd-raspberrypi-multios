d2e90f5b29059177901e5db9b44daf9b8ceb3655
raspberrypisig <raspberrypisig@gmail.com>
2020-10-23 03:06:01 +0000

    Update setup.sh

--- btrfs/setup.sh
+++ btrfs/setup.sh
@@ -2,7 +2,8 @@
 
 sudo apt install -y initramfs-tools btrfs-tools btrfs-progs
 echo btrfs >> /etc/initramfs-tools/modules	
-mkinitramfs -o /boot/initramfs-btrfs.gz
+VERSION=$(<pre>find /lib/modules   -name &apos;*v7l+&apos; -exec basename {} \;</pre>)
+mkinitramfs -o /boot/initramfs-btrfs.gz -v $VERSION
 # Disable kernel updates
 sudo apt-mark hold libraspberrypi-bin libraspberrypi-dev libraspberrypi-doc libraspberrypi0
 sudo apt-mark hold raspberrypi-bootloader raspberrypi-kernel raspberrypi-kernel-headers
