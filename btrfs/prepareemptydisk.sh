341b417b88083b8c4594bdbe2f32e5399ffe9618
raspberrypisig <raspberrypisig@gmail.com>
2020-10-25 12:17:09 +0000

    Update prepareemptydisk.sh

--- btrfs/prepareemptydisk.sh
+++ btrfs/prepareemptydisk.sh
@@ -9,26 +9,28 @@ mount|grep $usb_disk|awk '{print $1}'|xargs umount
 
 sfdisk -f $usb_disk <<EOF
 label: dos
-start=8192, size=200MiB, type=c
+,200MiB,c
 ,${secondpartitionsize}GiB,83
 ,,83
 EOF
 
-mkfs.vfat "${usb_disk}p1"
-mkfs.ext4 -F "${usb_disk}p2"
-#mkfs.vfat "${usb_disk}p2"
-mkfs.btrfs -f "${usb_disk}p3"
+partprobe "${usb_disk}"
+sleep 1
+mkfs.vfat "${usb_disk}1"
+mkfs.ext4 -F "${usb_disk}2"
+#mkfs.vfat "${usb_disk}2"
+mkfs.btrfs -f "${usb_disk}3"
 
 loop=$(losetup --show -Pf $raspbian_image)
 
 mkdir -p {p1,p2,usb1,usb2,usb3}
 mount -o ro ${loop}p1 p1
 mount -o ro ${loop}p2 p2
-mount ${usb_disk}p1 usb1
-mount ${usb_disk}p2 usb2
-mount ${usb_disk}p3 usb3
+mount ${usb_disk}1 usb1
+mount ${usb_disk}2 usb2
+mount ${usb_disk}3 usb3
 cd usb3
-btrfs subvolume create boot
+btrfs subvolume create @boot
 cd ..
 cp -r p1/* usb1
 rsync -a p2/ usb3/boot
