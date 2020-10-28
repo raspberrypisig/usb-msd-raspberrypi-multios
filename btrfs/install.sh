c8e6dde3fe16c66a8322510a7fbdade678e529fc
raspberrypisig <raspberrypisig@gmail.com>
2020-10-28 21:46:32 +0000

    Update install.sh

--- btrfs/install.sh
+++ btrfs/install.sh
@@ -1,3 +1,4 @@
 wget https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/raw/master/btrfs/prepareemptydisk.sh
 wget https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/raw/master/btrfs/preparestartos.sh
+wget https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/raw/master/btrfs/multipi4
 chmod +x *.sh
