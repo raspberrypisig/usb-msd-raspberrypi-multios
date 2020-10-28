85f560e4b42f3e5523168dcace563193f5a1a7c3
raspberrypisig <raspberrypisig@gmail.com>
2020-10-28 07:56:18 +0000

    Update OS-CHOOSER.sh

--- btrfs/distros/boot/linux/OS-CHOOSER.sh
+++ btrfs/distros/boot/linux/OS-CHOOSER.sh
@@ -4,6 +4,7 @@ set -x
 
 TEMP_DIR=/tmp/usb2
 USB_DISK=/dev/sda
+BTRFS_DIR=/tmp/usb3
 
 createsubvolumename() {
   name="$1"
@@ -22,11 +23,26 @@ then
     options+=("$line" "$line")
   done < $TEMP_DIR/oslist.txt
 fi
-umount /tmp/usb2
-rm -rf /tmp/usb2
 
 CHOICE=$(whiptail --title "Choose OS" --menu " "  --nocancel --noitem   20 70 5 "${options[@]}" 3>&1 1>&2 2>&3)
-osselection="${oslist[$CHOICE]}"
-echo $osselection
-volname=$(createsubvolumename $osselection)
+volname=$(createsubvolumename "$CHOICE")
 echo $volname
+
+if [ -d $TEMP_DIR/$volname ];
+then
+  rm -rf $TEMP_DIR/$volname
+fi
+
+mkdir $TEMP_DIR/$volname
+mkdir -p $BTRFS_DIR
+mount ${USB_DIR}3 $BTRFS_DIR
+cp $BTRFS_DIR/$volname/boot/config $TEMP_DIR
+echo "os_prefix=$volname" >> $TEMP_DIR/config.txt
+mkdir $TEMP_DIR/$volname
+cp -rv $BTRFS_DIR/boot/* $TEMP_DIR/$volname
+
+umount $BTRFS_DIR
+umount $TEMP_DIR
+rm -rf $BTRFS_DIR
+rm -rf $TEMP_DIR
+
