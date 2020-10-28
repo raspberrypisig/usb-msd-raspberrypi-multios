acbe2ba80b17409c3fedf1e9692898a33ec50cbe
raspberrypisig <raspberrypisig@gmail.com>
2020-10-28 22:38:53 +0000

    Update preparestartos.sh

--- btrfs/preparestartos.sh
+++ btrfs/preparestartos.sh
@@ -12,19 +12,20 @@ bootfiles=multios/btrfs/distros/boot/boot
 linuxfiles=multios/btrfs/distros/boot/linux
 setupscript=multios/btrfs/distros/boot/setup.sh
 
-apt install -y qemu qemu-user-static binfmt-support systemd-container git
+apt install -y qemu qemu-user-static binfmt-support systemd-container git subversion
 git clone https://github.com/raspberrypisig/usb-msd-raspberrypi-multios multios
 
 
-mkdir -p {usb1,usb3}
+mkdir -p {usb1,usb2,usb3}
 
 mount|grep $disk|awk '{print $1}'|xargs umount
 
 mount ${disk}1 usb1
+mount ${disk}2 usb2
 mount -t btrfs -o subvol=@boot ${disk}3 usb3
 
-cp -rv $bootfiles/* usb1
-rsync -av $linuxfiles/ usb3
+cp -r $bootfiles/* usb1
+rsync -a $linuxfiles/ usb3
 
 chmod +x $setupscript
 cp $setupscript usb3
@@ -33,6 +34,9 @@ cp usb3/initramfs-btrfs usb1
 rm usb3/initramfs-btrfs
 rm usb3/setup.sh
 
+svn co https://github.com/raspberrypi/firmware/trunk/boot
+cp boot/* usb2
+rm -rf boot
 
-umount {usb1,usb3}
+umount {usb1,usb2,usb3}
 rm -rf {usb1,usb3,multios}
