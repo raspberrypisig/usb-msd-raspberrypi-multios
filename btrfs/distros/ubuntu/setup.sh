e35c4a0816d79781a4760e0cdfd7be35fedc914e
raspberrypisig <raspberrypisig@gmail.com>
2020-10-30 06:01:16 +0000

    Update setup.sh

--- btrfs/distros/ubuntu/setup.sh
+++ btrfs/distros/ubuntu/setup.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
+set -x
 
+volname="$1"
+
+apt update
+apt install -y initramfs-tools btrfs-tools btrfs-progs
+
+echo btrfs >> /etc/initramfs-tools/modules
+
+VERSION=$(find /lib/modules -name *v7l+ -exec basename {} \; )
+mkinitramfs -o /boot/initramfs-btrfs -v $VERSION
+
+sed -i "s/PLACEHOLDER/$volname/" /boot/cmdline.txt 
+sed -i "s/PLACEHOLDER/$volname/" /etc/fstab
+
+# Disable kernel updates
+sudo apt-mark hold libraspberrypi-bin libraspberrypi-dev libraspberrypi-doc libraspberrypi0
+sudo apt-mark hold raspberrypi-bootloader raspberrypi-kernel raspberrypi-kernel-headers
+
+systemctl disable resize2fs_once
+systemctl disable dphys-swapfile
