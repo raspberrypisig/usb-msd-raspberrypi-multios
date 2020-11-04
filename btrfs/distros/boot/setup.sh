bac6ab4441cced5cfee40a9fe57060280c8d285b
raspberrypisig <raspberrypisig@gmail.com>
2020-11-04 15:14:49 +1100

    Update setup.sh

--- btrfs/distros/boot/setup.sh
+++ btrfs/distros/boot/setup.sh
@@ -9,7 +9,7 @@ mkinitramfs -o /initramfs-btrfs -v $VERSION
 apt-mark hold libraspberrypi-bin libraspberrypi-dev libraspberrypi-doc libraspberrypi0
 apt-mark hold raspberrypi-bootloader raspberrypi-kernel raspberrypi-kernel-headers
 apt remove -y --purge openssh-server
-
+raspi-config nonint do_configure_keyboard us
 
 services_disable=("resize2fs_once" "networking" "dhcpcd" "rpi-eeprom-update" "avahi-daemon" "dphys-swapfile" "wpa_supplicant" "rc-local" )
 services_enable=("oschooser")
