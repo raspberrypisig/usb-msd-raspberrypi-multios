4496919a0470461be81dbbc342867a76d9d08330
raspberrypisig <raspberrypisig@gmail.com>
2020-11-06 06:38:15 +0000

    Update setup.sh

--- btrfs/distros/ubuntu/setup.sh
+++ btrfs/distros/ubuntu/setup.sh
@@ -9,7 +9,7 @@ apt install -y initramfs-tools btrfs-tools btrfs-progs
 echo btrfs >> /etc/initramfs-tools/modules
 
 VERSION=$(find /lib/modules -name '*-raspi' -exec basename {} \; )
-mkinitramfs -o /boot/initramfs-btrfs -v $VERSION
+update-initramfs -c -k $VERSION
 
 sed -i "s/PLACEHOLDER/$volname/" /boot/cmdline.txt 
 sed -i "s/PLACEHOLDER/$volname/" /etc/fstab
