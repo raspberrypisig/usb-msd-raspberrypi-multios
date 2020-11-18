88a4ce1a6c25c531d314694e8fa1313f03b31299
raspberrypisig <raspberrypisig@gmail.com>
2020-11-18 12:34:43 +0000

    Update setup.sh

--- btrfs/distros/ubuntu/setup.sh
+++ btrfs/distros/ubuntu/setup.sh
@@ -3,11 +3,11 @@ set -x
 
 volname="$1"
 
-mv /etc/resolv.conf /etc/resolv.conf.old
-echo nameserver 8.8.8.8 > /etc/resolv.conf
+#mv /etc/resolv.conf /etc/resolv.conf.old
+#echo nameserver 8.8.8.8 > /etc/resolv.conf
 
-apt update
-apt install -y initramfs-tools btrfs-progs
+#apt update
+#apt install -y initramfs-tools btrfs-progs
 
 echo btrfs >> /etc/initramfs-tools/modules
 
@@ -18,6 +18,6 @@ cp /boot/initrd.img-$VERSION /boot/firmware/initrd.img
 sed -i "s/PLACEHOLDER/$volname/" /boot/firmware/cmdline.txt 
 sed -i "s/PLACEHOLDER/$volname/" /etc/fstab
 
-mv /etc/resolv.conf.old /etc/resolv.conf
+#mv /etc/resolv.conf.old /etc/resolv.conf
 
 apt-mark hold linux-headers-$VERSION linux-modules-$VERSION linux-image-$VERSION
