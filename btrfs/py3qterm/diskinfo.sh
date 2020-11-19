de9feec8a2eb4ea9482c6b73ca39da064e9495f1
raspberrypisig <raspberrypisig@gmail.com>
2020-11-19 01:07:49 +0000

    Update diskinfo.sh

--- btrfs/py3qterm/diskinfo.sh
+++ btrfs/py3qterm/diskinfo.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-set -x
+#set -x
 
-usbdevices=$(readlink -f /dev/disk/by-id/usb*|sed -r  '/[0-9]+/d'|sort)
+usbdevices=$(readlink -f /dev/disk/by-id/{usb*,ata*}|sed -r  '/[0-9]+/d'|sort)
 
 for usbdisk in $usbdevices
 do
   diskinfo=$(udevadm info $usbdisk)
-  capacity=$(fdisk -l /dev/sdd|head -n1|awk '{print $3 " " $4}'|cut -f1 -d',')
+  capacity=$(fdisk -l $usbdisk|head -n1|awk '{print $3 " " $4}'|cut -f1 -d',')
   
-  echo $diskinfo
+  echo $usbdisk
+  #echo $diskinfo
   echo $capacity
   
 done
