66d20d1617a81c14673f6cacce62166be35e0b51
raspberrypisig <raspberrypisig@gmail.com>
2020-11-19 09:44:32 +0000

    Create building.sh

---
+++ btrfs/py3qterm/building.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt install -y git build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+curl https://pyenv.run | bash
+
+cat<<EOF  > ~/.bashrc
+export PATH="/root/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+EOF
+
+source ~/.bashrc
+
+cd /mnt
+mkdir test
+cd test
+env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.6.8
+pyenv local 3.6.8
+python --version
