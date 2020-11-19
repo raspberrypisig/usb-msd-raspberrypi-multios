3c052f7a79e182358964818c8c99c6287455264b
raspberrypisig <raspberrypisig@gmail.com>
2020-11-19 01:18:30 +0000

    Update diskinfo.sh

--- btrfs/py3qterm/diskinfo.sh
+++ btrfs/py3qterm/diskinfo.sh
@@ -8,8 +8,6 @@ for usbdisk in $usbdevices
 do
   model=$(udevadm info $usbdisk|grep ID_MODEL=|cut -f2- -d'=')
   vendor=$(udevadm info $usbdisk|grep ID_VENDOR=|cut -f2- -d'=')
-  
   capacity=$(fdisk -l $usbdisk|head -n1|awk '{print $3 " " $4}'|cut -f1 -d',')
-  
   echo "$vendor $model $capacity"
 done
