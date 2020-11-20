cc5e242327106e6c411179ccec4cfea11e04d206
raspberrypisig <raspberrypisig@gmail.com>
2020-11-20 10:04:20 +0000

    Update main.py

--- btrfs/py3qterm/AppImageBuild/main.py
+++ btrfs/py3qterm/AppImageBuild/main.py
@@ -4,7 +4,7 @@ import sys
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('MainWindow.ui', self) # Load the .ui file
+        uic.loadUi('mainwindow.ui', self) # Load the .ui file
         self.show() # Show the GUI
 
 app = QtWidgets.QApplication(sys.argv)
