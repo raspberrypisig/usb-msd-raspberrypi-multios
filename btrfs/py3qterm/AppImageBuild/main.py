5c11b2832563ee1a89a267c9119be5c115e17533
raspberrypisig <raspberrypisig@gmail.com>
2020-11-20 10:41:48 +0000

    Update main.py

--- btrfs/py3qterm/AppImageBuild/main.py
+++ btrfs/py3qterm/AppImageBuild/main.py
@@ -1,11 +1,28 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
-class Ui(QtWidgets.QMainWindow):
+form_1, base_1 = uic.loadUiType('mainwindow.ui')
+form_2, base_2 = uic.loadUiType('choosedisk.ui')
+
+import subprocess
+
+output = subprocess.check_output(['bash', 'diskinfo.sh'])
+print(output)
+
+
+class ChooseDiskDialog(base_2, form_2):
     def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('mainwindow.ui', self) # Load the .ui file
+        super(base_2, self).__init__()
+        self.setupUi(self)
+
+
+class Ui(base_1, form_1):
+    def __init__(self):
+        super(base_1, self).__init__()
+        self.setupUi(self)
         self.show() # Show the GUI
+        dialog = ChooseDiskDialog()
+        dialog.exec_()
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
