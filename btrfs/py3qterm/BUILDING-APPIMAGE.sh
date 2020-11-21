985192f53a950cbd131a940890c86f1e287cc852
raspberrypisig <raspberrypisig@gmail.com>
2020-11-21 07:40:51 +0000

    Update BUILDING-APPIMAGE.sh

--- btrfs/py3qterm/BUILDING-APPIMAGE.sh
+++ btrfs/py3qterm/BUILDING-APPIMAGE.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
+rm -rf AppImageBuild
 apt install -y python3-pip python3-setuptools patchelf desktop-file-utils libgdk-pixbuf2.0-dev fakeroot subversion git
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /usr/local/bin/appimagetool
 chmod +x /usr/local/bin/appimagetool
 pip3 install git+https://github.com/AppImageCrafters/appimage-builder.git
 svn co https://github.com/raspberrypisig/usb-msd-raspberrypi-multios/trunk/btrfs/py3qterm/AppImageBuild
 cd AppImageBuild
-appimage-builer --skip-tests
+appimage-builder --skip-tests
 
