dec9c539e4937e26de4dcafa95727cee3bdb04bc
raspberrypisig <raspberrypisig@gmail.com>
2020-11-29 02:47:09 +0000

    Create diskinfo.sh

---
+++ btrfs/yad/diskinfo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#set -x
+
+usbdevices=$(readlink -f /dev/disk/by-id/{usb*,ata*}|sed -r  '/[0-9]+/d'|sort)
+
+for usbdisk in $usbdevices
+do
+  model=$(udevadm info $usbdisk|grep ID_MODEL=|cut -f2- -d'=')
+  vendor=$(udevadm info $usbdisk|grep ID_VENDOR=|cut -f2- -d'=')
+  capacity=$(fdisk -l $usbdisk|head -n1|awk '{print $3 " " $4}'|cut -f1 -d',')
+  if [ ! $vendor ];
+  then
+  echo "${usbdisk}: $model $capacity"  
+  else
+  echo "${usbdisk}: $vendor $model $capacity"
+  fi
+done
