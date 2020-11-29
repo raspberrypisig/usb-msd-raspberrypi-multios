05fd445c141850da08f18e87b3bee831461df58e
raspberrypisig <raspberrypisig@gmail.com>
2020-11-29 03:36:32 +0000

    Update 3.sh

--- btrfs/yad/3.sh
+++ btrfs/yad/3.sh
@@ -2,4 +2,13 @@
 
 disk="$1"
 
-yad --text "Raspbian Image eg.2020-raspbian.img" --text-align=center --form --field="Folder List:SFL"
+output=$(yad --text "Raspbian Image eg.2020-raspbian.img" --text-align=center --form --field="Raspbian Image:SFL")
+buttonpressed=$?
+
+if [ $buttonpressed -eq 0 ];
+then
+raspbianimg=$(echo $output|sed  's/|//')
+echo multipi4-preparedisk "$raspbianimg" "disk"
+else
+exit 1
+fi
