54646f836507a6bb5809fa370b52cf5f4d7a68d0
raspberrypisig <raspberrypisig@gmail.com>
2020-11-29 02:51:25 +0000

    Update 1.sh

--- btrfs/yad/1.sh
+++ btrfs/yad/1.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-LIST="one\ntwo\nthree"
-output="$(echo -e "$LIST" | yad --center --borders=30 --title='Pi-Apps' --width=600 --height=400 --no-headers --buttons-layout=center  \
+#LIST="one\ntwo\nthree"
+# echo -e "$LIST"
+output="$(bash diskinfo.sh | yad --center --borders=30 --title='Pi-Apps' --width=600 --height=400 --no-headers --buttons-layout=center  \
         --text="\nPlease select drive to make into a multiboot Pi disk\n" --text-align=center  \
         --list --separator='\n'  \
         --column=:Name --print-column=1 \
@@ -10,3 +11,4 @@ output="$(echo -e "$LIST" | yad --center --borders=30 --title='Pi-Apps' --width=
         )"
 buttonpressed=$?
 echo $buttonpressed
+echo $output
