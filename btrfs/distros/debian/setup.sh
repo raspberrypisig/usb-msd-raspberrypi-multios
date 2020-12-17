548fddfd27ef8cda734ad5756f866302e057ef9d
Mohan Gupta <raspberrypisig@gmail.com>
2020-12-18 09:19:13 +1100

    debian 10 mods

--- btrfs/distros/debian/setup.sh
+++ btrfs/distros/debian/setup.sh
@@ -2,3 +2,13 @@
 set -x
 
 volname="$1"
+
+sed -i "s/PLACEHOLDER/$volname/" /boot/cmdline.txt 
+sed -i "s/PLACEHOLDER/$volname/" /etc/fstab
+
+# Disable kernel updates
+sudo apt-mark hold libraspberrypi-bin libraspberrypi-dev libraspberrypi-doc libraspberrypi0
+sudo apt-mark hold raspberrypi-bootloader raspberrypi-kernel raspberrypi-kernel-headers
+
+systemctl disable resize2fs_once
+systemctl disable dphys-swapfile
