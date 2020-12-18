ec5c4218836c3fccef26b1efeb0e9ca65732e59f
Mohan Gupta <raspberrypisig@gmail.com>
2020-12-18 15:41:21 +1100

    debian 10 resume from boot

--- btrfs/distros/debian/setup.sh
+++ btrfs/distros/debian/setup.sh
@@ -8,10 +8,30 @@ sed -i "s/PLACEHOLDER/$volname/" /etc/fstab
 
 VERSION=$(find /lib/modules -name *-arm64 -exec basename {} \; )
 
+echo "RESUME=none" > /etc/initramfs-tools/conf.d/resume
+
 apt-mark hold linux-image-$VERSION
 apt update
 apt install -y network-manager apparmor
-#DEBIAN_FRONTEND=noninteractive apt install -y task-kde-desktop
+
+cat<<EOF > /etc/NetworkManager/NetworkManager.conf
+[main]
+dns=default
+plugins=ifupdown,keyfile
+autoconnect-retries-default=0
+rc-manager=file
+dhcp=internal
+[ifupdown]
+managed=true
+[device]
+wifi.scan-rand-mac-address=no
+[keyfile]
+unmanaged-devices=type:bridge;type:tun;type:veth
+[logging]
+backend=journal
+EOF
+
+
 
 
 
