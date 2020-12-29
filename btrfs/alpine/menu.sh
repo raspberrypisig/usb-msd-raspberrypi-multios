10149deb56dc572111fbfdaad85960aa1604a17b
Mohan Gupta <raspberrypisig@gmail.com>
2020-12-30 10:53:45 +1100

    apks directory

--- btrfs/alpine/menu.sh
+++ btrfs/alpine/menu.sh
@@ -2,6 +2,6 @@
 
 # This file lives in /etc/local.d/menu.sh inside localhost.apkovl.tar.gz
 
-cd /media/usb/apks/aarch64
+cd /media/sda1/apks/aarch64
 apk add --force-non-repository --allow-untrusted ./popt-*apk ./pcre-*.apk ./ncurses-*.apk ./readline-*.apk ./slang-*.apk ./bash-*.apk ./newt-*.apk
 /etc/local.d/oschooser.sh
